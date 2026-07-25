6533eaa70cd5b086878fefeb9633c5a5fd60f5bd
Eduardo Ferreira Martins <eduardoferreiramartins74@gmail.com>
2026-07-25 00:21:47 -0300

    chore: update CSP directives for Google services
    Add google.com and gstatic.com to scriptSrc and frameSrc to support embedded Google services.

--- server.ts
+++ server.ts
@@ -137,12 +137,12 @@ app.use(helmet({
   contentSecurityPolicy: {
     directives: {
       defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", "https://apis.google.com", "https://www.gstatic.com"],
+      scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", "https://apis.google.com", "https://www.gstatic.com", "https://www.google.com"],
       styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
       fontSrc: ["'self'", "https://fonts.gstatic.com"],
       imgSrc: ["'self'", "data:", "https:", "blob:"],
       connectSrc: ["'self'", "https://api.stripe.com", "https://maps.googleapis.com", "https://wa.me", "https://identitytoolkit.googleapis.com", "https://securetoken.googleapis.com", "https://www.googleapis.com"],
-      frameSrc: ["'self'", "https://js.stripe.com", "https://accounts.google.com", "https://*.firebaseapp.com"],
+      frameSrc: ["'self'", "https://js.stripe.com", "https://accounts.google.com", "https://*.firebaseapp.com", "https://www.google.com", "https://www.gstatic.com"],
       mediaSrc: ["'self'", "https://assets.mixkit.co"],
     },
   },
